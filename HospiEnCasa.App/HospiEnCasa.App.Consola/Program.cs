2bcd5710938333f773619b72a0e6b6f8996aa9e0
juseji94 <juseji94@gmail.com>
2022-07-03 20:56:08 -0500

    Agragados repositorio de paciente en capa de persistencia y actualizado Program de consola para añadir paciente.

--- HospiEnCasa.App/HospiEnCasa.App.Consola/Program.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Consola/Program.cs
@@ -1,13 +1,33 @@
 ﻿using System;
 using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
 
 namespace HospiEnCasa.App.Consola
 {
     class Program
     {
+        private static IRepositorioPaciente _repoPaciente = new RepositorioPaciente(new Persistencia.AppContext());
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! Entity Framework+");
+            Console.WriteLine("Hello World! Entity Framework");
+            AddPaciente();
+        }
+        private static void AddPaciente()
+        {
+            var paciente = new Paciente
+            {
+                Nombre = "Pepito",
+                Apellidos = "Perez Prieto",
+                NumeroTelefono = "3000000000",
+                Genero = Genero.masculino,
+                Direccion = "Calle 8",
+                Longitud = 9.54F,
+                Latitud = 45.55F,
+                Ciudad = "Santa Marta",
+                FechaNacimiento = new DateTime(1972, 11, 08)
+            };
+            _repoPaciente.AddPaciente(paciente);
         }
     }
 }
