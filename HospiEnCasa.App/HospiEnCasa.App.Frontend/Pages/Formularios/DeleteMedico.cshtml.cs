94cc560a2fdc56353d4a3a43f1a29e11d847068b
jarallo <julian-jllo@hotmail.com>
2022-07-15 19:13:25 -0500

    CRUD medico

---
+++ HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Formularios/DeleteMedico.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HospiEnCasa.App.Frontend
+{
+    public class DeleteMedicoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
