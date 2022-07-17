528abad267cb9b032dbdf5ac7ee3ea7edd29b413
paula-cano <paula.14canob@gmail.com>
2022-07-16 21:21:02 -0500

    cambios en la lista pacientes; frontend y backend

--- HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Formularios/ListPacientes.cshtml.cs
+++ HospiEnCasa.App/HospiEnCasa.App.Frontend/Pages/Formularios/ListPacientes.cshtml.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HospiEnCasa.App.Dominio;
+using HospiEnCasa.App.Persistencia;
+
 
 namespace HospiEnCasa.App.Frontend.Pages
 {
     public class ListPacientesModel : PageModel
     {
+        private IRepositorioPaciente repositorioPaciente = new RepositorioPaciente(new Persistencia.AppContext());
+        public IEnumerable<Paciente> Pacientes {get; set;}
+
         public void OnGet()
         {
+            Pacientes = repositorioPaciente.GetAllPacientes();
         }
     }
 }
